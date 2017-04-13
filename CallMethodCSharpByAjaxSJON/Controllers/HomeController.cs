6d86bb6287d2e1022bc8963a6ba0f71f952cc42f
Rodrigo Aires de Oliveira <rodrigo.aires@gmail.com>
2017-04-13 09:55:32 -0400

    commit

---
+++ CallMethodCSharpByAjaxSJON/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Services;
+
+namespace MvcApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        [WebMethod]
+        [AllowAnonymous]
+        public ActionResult AjaxMethod(string contentParam)
+        {
+            //do anything
+            return Json("You sent: " + contentParam, JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
